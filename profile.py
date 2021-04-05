bebdbd9a875bc07cf5ccbc4208649891e51600ac
Linh B Ngo <lngo@wcupa.edu>
2021-04-05 10:13:30 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -4,8 +4,9 @@ import geni.rspec.pg as rspec
 # Create a Request object to start building the RSpec.
 request = portal.context.makeRequestRSpec()
 # Create a XenVM
-node = request.XenVM("node")
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
+node = request.RawPC("node")
+#urn:publicid:IDN+utah.cloudlab.us+image+emulab-ops//UBUNTU20-64-ARM
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-ARM"
 node.routable_control_ip = "true"
 
 node.addService(rspec.Execute(shell="/bin/sh",
