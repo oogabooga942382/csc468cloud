81f540b372ed778797399b8904df4557d6928165
Linh B Ngo <lngo@wcupa.edu>
2021-04-18 17:56:51 -0400

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -5,4 +5,7 @@ sudo apt install -y python3 python3-pip default-jdk
 sudo pip3 install ansible 
 sudo pip3 install openshift
 ansible-galaxy install geerlingguy.jenkins
+sudo usermod -aG docker jenkins
+sudo chmod 777 /var/run/docker.sock
+
 #ansible-galaxy install geerlingguy.docker
