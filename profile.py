9a28a28904b282151832096f119b74c39aef41a0
oogabooga942382 <zacharyromereeves@gmail.com>
2021-04-25 07:41:41 -0400

    update prof.py

--- profile.py
+++ profile.py
@@ -34,13 +34,14 @@ for i in range(num_nodes):
   iface.component_id = "eth1"
   iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
   link.addInterface(iface)
-
+  
+  # setup Docker
+  node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
+  # setup Kubernetes
   node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_kubernetes.sh"))
   node.addService(pg.Execute(shell="sh", command="sudo swapoff -a"))
-  node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
-
+  
   if i == 0:
-    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_jenkins.sh"))
     node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/kube_manager.sh"))
   else:
     node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/kube_worker.sh"))
